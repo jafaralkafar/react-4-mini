b05037d023f67a06e665dab9ca362fcd8a4ffe85
Grant Fox <granttfox@gmail.com>
2019-06-04 13:19:12 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import Nav from "./Components/Nav/Nav";
 import "./App.css";
+import { HashRouter as Router } from 'react-router-dom'
+
+import routes from './routes'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Nav />
-        <h1 style={{ padding: "200px 35%" }}>
-          This is where your pages will appear
-        </h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Nav />
+          {routes}
+        </div>
+      </Router>
     );
   }
 }
